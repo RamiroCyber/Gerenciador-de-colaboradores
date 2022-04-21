705022e02651def0f4b3098a7e769d0f73493695
RamiroCyber <ramiro.drgmail.com>
2022-04-21 13:06:21 -0300

    exception

---
+++ src/main/java/com/tech/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.tech.employeemanager.repository;
+
+import com.tech.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ src/main/java/com/tech/employeemanager/service/EmployeeService.java
@@ -0,0 +1,40 @@
+package com.tech.employeemanager.service;
+
+import com.tech.employeemanager.exception.UserNotFoundException;
+import com.tech.employeemanager.model.Employee;
+import com.tech.employeemanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private  EmployeeRepository employeeRepository;
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public Optional<Employee> findEmployeeById(Long id){
+        return Optional.ofNullable(employeeRepository.findById(id).orElseThrow(() -> new UserNotFoundException("Usuario " + id + "não encontrado")));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepository.deleteById(id);
+    }
+
+
+}
